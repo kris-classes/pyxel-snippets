633b4957469df75fcbb5976a1822024b8f215210
Kris Pritchard <krisrp@users.noreply.github.com>
2020-04-12 00:15:27 +1200

    Add classes snippet

---
+++ 04_classes.py
@@ -0,0 +1,26 @@
+"""
+Instead of using functions and global variables, let's store state using classes.
+
+Kris Pritchard / @krisrp
+"""
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.cls(0)
+
+        # Give run the update/draw callbacks
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.text(80, 80, "Hello World from a class!", 9)
+
+
+
+if __name__ == "__main__":
+    App()
